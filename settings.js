3c13b0d9aad965500e288c1367bc520842a01aaa
earthsaf <154933783+earthsaf@users.noreply.github.com>
2024-08-24 04:24:46 +0100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,7 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+
+global.owner = ['2347082252014','2349150538944','2349167632397']
+global.botname = 'Dux Bot'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "Dux Official"
+global.sticker2 = "🌜"
